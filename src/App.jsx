0b3408cd4b854cba1a2da1b23b68e9aac8580dd4
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-04-07 14:02:56 +0600

    complete my blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,58 @@
 import './App.css'
 import Navber from './components/Navber/Navber'
 import Blogs from './components/Blogs/Blogs'
+import { useState } from 'react'
 
 function App() {
+
+  const [bookmarked , setBookmarked] = useState([]);
+
+  const handleBookMark = (blog) => {
+
+   setBookmarked([...bookmarked , blog]);
+
+  }
+
+  // console.log(bookmarked);
+
+  const [bookmarkedtime , setBookmarkedtime] = useState(0);
+
+  const handleBookMarkTime = (time , id) => {
+
+    setBookmarkedtime(bookmarkedtime + time);
+
+    handleRemoveFromBookmark(id);
+  }
+
+  // console.log(bookmarkedtime);
+
+  const handleRemoveFromBookmark = (id) => {
+
+    const remainingBookmark = bookmarked.filter(mark => mark.id !== id)
+
+    setBookmarked(remainingBookmark);
+
+  }
 
   return (
     <>
      <Navber></Navber>
 
-
-
      <div className="main-container flex text-center">
       <div className="left-container w-[70%]">
 
-          <Blogs></Blogs>
+          <Blogs handleBookMark = {handleBookMark} handleBookMarkTime = {handleBookMarkTime}></Blogs>
       </div>
 
       <div className="right-container w-[30%]">
-        <h1>Reading time : 0</h1>  
-        <h1>Bookmarked Count : 0</h1>  
+        <h1>Reading time : {bookmarkedtime}</h1>  
+        <h1>Bookmarked Count : {bookmarked.length}</h1>  
+
+
+        {
+          bookmarked.map(marked => <p key={marked.id} className='bg-amber-400 shadow-2xl border-2 border-amber-600 p-2 text-black rounded-xl mb-1'>{marked.title}</p>)
+        }
+
       </div>
      </div>
     </>
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({blog}) => {
+const Blog = ({blog , handleBookMark , handleBookMarkTime}) => {
 
-    console.log(blog);
+    // console.log(blog);
+    // console.log(handleBookMark);
+    // console.log(handleBookMarkTime);
 
     return (
         <div className='m-2'>
@@ -19,18 +21,18 @@ const Blog = ({blog}) => {
       <div className='flex items-center justify-between gap-5'>
         <p className='text-xl font-bold'>{blog.author}</p>
         <img className='w-16 rounded-full' src={blog.author_img} alt="" />
-        <FaBookmark size={20}/>
+        <button onClick={() => handleBookMark(blog)}><FaBookmark size={20}/></button>
       </div>
     <p>{blog.posted_date}</p>
 
       <div className='flex gap-5 border-4 border-blue-900 p-2 rounded-2xl'>
         {
-          blog.hashtags.map(has => <p>{has}</p>)
+          blog.hashtags.map(has => <p key={has}>{has}</p>)
         }
       </div>
 
     <div className="card-actions">
-      <button className="btn btn-primary">Mark as read</button>
+      <button onClick={() => handleBookMarkTime(blog.reading_time , blog.id)} className="btn btn-primary">Mark as read</button>
     </div>
   </div>
 </div>
